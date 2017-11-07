aa8d1a982d0b70f490661c9c02dec5e3997a78de
梁煜铭 <yuming_liang@daydao.com>
2017-11-07 14:53:13 +0800

    修复了一下,不使用remove的方式来完成数据的更新

--- main.go
+++ main.go
@@ -5,23 +5,23 @@ import (
 	"fmt"
 	"os"
 	"net"
-	"time"
+	"net/http"
 )
 
 func main() {
-	//server := domain.Server{ListenPort: 8888, ClientList: make([]domain.Client, 0)}
-	//http.HandleFunc("/getClient", server.GetAllClients)
-	//go http.ListenAndServe(":8080", nil)
-	//
-	//server.StartServer()
-	ticket := time.NewTicker(time.Second * 20)
-	func() {
-		for _ = range ticket.C {
-			client := domain.Client{ClientName: "bbbbb", ClientAddr: getAddr().(*net.IPNet).IP.String()}
-			fmt.Println(client)
-			client.MakeDiscover()
-		}
-	}()
+	server := domain.Server{ListenPort: 8888, ClientList: make([]domain.Client, 0)}
+	http.HandleFunc("/getClient", server.GetAllClients)
+	go http.ListenAndServe(":8080", nil)
+
+	server.StartServer()
+	//ticket := time.NewTicker(time.Second * 20)
+	//func() {
+	//	for _ = range ticket.C {
+	//		client := domain.Client{ClientName: "bbbbb", ClientAddr: getAddr().(*net.IPNet).IP.String()}
+	//		fmt.Println(client)
+	//		client.MakeDiscover()
+	//	}
+	//}()
 }
 func getAddr() net.Addr {
 	addrs, err := net.InterfaceAddrs()
