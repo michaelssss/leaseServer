5dde1608f0f55224d987a5a369982b53c64e159b
梁煜铭 <yuming_liang@daydao.com>
2017-12-22 09:34:56 +0800

    增加通过query传递accessKey

--- server/HttpServer.go
+++ server/HttpServer.go
@@ -17,11 +17,16 @@ type MyHandleHttp interface {
 
 func (myHandleHttp myHandleHttpStuct) HandleHttp(w http.ResponseWriter, req *http.Request) {
 	accessKey := req.Header.Get("accessKey")
+	if "" == accessKey {
+		accessKey = req.URL.Query().Get("accessKey")
+	}
 	if accessKey == myHandleHttp.key {
-		respString := jsonResp{IP: req.Header.Get("X-real-ip")}
+		ip1 := req.Header.Get("X-real-ip")
+		if "" == ip1 {
+			ip1 = req.RemoteAddr
+		}
+		respString := jsonResp{IP: ip1}
 		jsonbyte, _ := json.Marshal(respString)
-		header:=w.Header()
-		header.Add("hello","world")
 		w.Write(jsonbyte)
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
